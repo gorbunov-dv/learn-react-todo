1bfcea797626df5215cbd383a008431543b65ef7
Dmitry <gorbunov.webdev@gmail.com>
2018-03-09 08:14:10 +0400

    Fixed bags

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="todo-wrapper">
-          Hello
+          <Header />
         </div>
       </div>
     );
