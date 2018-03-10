58bfc3ac465015179d22ffa068ad52f471122c99
Dmitry <gorbunov.webdev@gmail.com>
2018-03-11 02:19:09 +0400

    Added TodoItem css file

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,4 +9,12 @@ export default class TodoItem extends React.Component {
     removeTodo(id) {
         this.props.removeTodo(id);
     }
+
+    render() {
+        return (
+            <div className="todoWrapper">
+                <button className="removeTodo" onClick={(e) => this.removeTodo(this.props.id)}>remove</button>
+            </div>
+        );
+    }
 }
