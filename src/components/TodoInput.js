4a750a4a43db9acaf81bd4836c8a6ca86202c6e2
Dmitry <gorbunov.webdev@gmail.com>
2018-03-10 23:55:19 +0400

    Fixed bags

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -5,24 +5,28 @@ export default class TodoInput extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {value: "test"};
+        this.state = {value: ''};
 
         this.handleChang = this.handleChange.bind(this);
         this.addTodo = this.addTodo.bind(this);
     }
 
     handleChange(e) {
-        console.log("Changing text");
+        this.setState({value: e.target.value});
     }
 
     addTodo(todo) {
-        console.log("TODO: ", todo);
+        // Ensure the todo text isn't empty
+        if (todo.length > 0) {
+            this.props.addTodo(todo);
+            this.setState({value: ''});
+        }
     }
 
     render() {
         return (
             <div>
-                <input type="text" value="" onChange={this.handleChange} />
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
                 <button className="btn btn-primary" onClick={() => this.addTodo(this.state.value)}>Submit</button>
             </div>
         );
